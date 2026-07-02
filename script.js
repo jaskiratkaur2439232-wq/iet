81c41b0caaa46cc4f71b054200b6820ad7d37383
jaskirat <jaskiratkaur2439232@gmail.com>
2026-07-02 18:33:12 +0530

    bhaddal done

---
+++ script.js
@@ -0,0 +1,14 @@
+// for(let i=10; i>0 ;i--){
+//     console.log(i)
+// }
+// for( let i=1;i<21;i++){
+//     if(i%2===0){
+//         console.log(i)
+//     }
+// }
+// let i=1;
+// while(i<16){
+//     if(i%2===1){
+//         console.log(i)
+//     }
+// }
